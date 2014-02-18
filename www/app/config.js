ca763b48a837d3ba368cc9e8e6cdfb8d745bac4c
Sven Lito <me@svenlito.com>
2014-02-18 17:51:41 +0100

    [fix] urlify

--- www/app/config.js
+++ www/app/config.js
@@ -17,7 +17,7 @@ require.config({
     validation:    '/lib/backbone-validation/dist/backbone-validation',
     hoodie:        '/lib/hoodie/dist/hoodie',
     backboneHoodie:'/lib/backbone-hoodie/src/backbone-hoodie',
-    urlify:        '/lib/urlify.js/index'
+    urlify:        '/lib/urlify/index'
   },
 
   shim: {
