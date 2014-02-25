dce36b391f318e9486b8fe85126b35ebb3b5cf09
Alex Feyerke <kontakt@momoi.de>
2014-02-25 23:21:28 +0100

    [wip] Slip finally fucking works

--- www/app/views/answersList.js
+++ www/app/views/answersList.js
@@ -38,14 +38,47 @@ function (app, Marionette, tmpl, Slip) {
     tagName: 'ul',
     id: 'slipList',
     itemView: Answer,
-    onRender: function() {
+    onDomRefresh: function() {
       var self = this;
 
       console.log("answersList has rendered");
       console.log("afterRender: ", $('#slipList').length);
       if($('#slipList').length !== 0){
         console.log("Slip attached!");
-        new Slip($('#slipList')[0]);
+        //new Slip($('#slipList')[0]);
+
+
+        var ol = document.getElementById('slipList');
+        ol.addEventListener('slip:beforereorder', function(e){
+          if (/demo-no-reorder/.test(e.target.className)) {
+            e.preventDefault();
+          }
+        }, false);
+
+        ol.addEventListener('slip:beforeswipe', function(e){
+          if (e.target.nodeName === 'INPUT' || /demo-no-swipe/.test(e.target.className)) {
+            e.preventDefault();
+          }
+        }, false);
+
+        ol.addEventListener('slip:beforewait', function(e){
+          if (e.target.className.indexOf('instant') > -1) {
+            e.preventDefault();
+          }
+        }, false);
+
+        ol.addEventListener('slip:afterswipe', function(e){
+          e.target.parentNode.appendChild(e.target);
+        }, false);
+
+        ol.addEventListener('slip:reorder', function(e){
+          e.target.parentNode.insertBefore(e.target, e.detail.insertBefore);
+          return false;
+        }, false);
+
+        new Slip(ol);
+
+
       }
     }
 
